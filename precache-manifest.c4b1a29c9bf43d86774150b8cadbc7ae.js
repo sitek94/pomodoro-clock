c41a605827626f989bf534d41f41560f710b2cbb
Maciek Sitkowski <msitkowski94@gmail.com>
2020-08-26 19:49:35 +0200

    Updates

---
+++ precache-manifest.c4b1a29c9bf43d86774150b8cadbc7ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a2639d4eb52745203c087d7a9b7be4",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "bb8eca737a0167e81af4",
+    "url": "/pomodoro-clock/static/js/2.41805ceb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pomodoro-clock/static/js/2.41805ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba26169af5cfc621d20",
+    "url": "/pomodoro-clock/static/js/main.193d3e61.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  },
+  {
+    "revision": "76f8bf516bbcd009c7eb3f958b26cbb4",
+    "url": "/pomodoro-clock/static/media/gong.76f8bf51.wav"
+  }
+]);
